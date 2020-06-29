57222d995aa65186a738a5f39fca04958bbd8f00
junior <junior.moraisgti@gmail.com>
2020-06-29 19:28:46 -0300

    Criando o service live e o componente liveList

--- agendalive/src/app/app.module.ts
+++ agendalive/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './views/home/home.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { LiveListComponent } from './views/home/live-list/live-list.component';
 
 @NgModule({
@@ -18,7 +21,10 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
