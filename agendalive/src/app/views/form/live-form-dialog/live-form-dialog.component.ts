26eb8e055315b41ba9a8f8c2f8bb5f13b7bdf2a9
junior <junior.moraisgti@gmail.com>
2020-08-04 17:25:04 -0300

    Correção liveForm

--- agendalive/src/app/views/form/live-form-dialog/live-form-dialog.component.ts
+++ agendalive/src/app/views/form/live-form-dialog/live-form-dialog.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-live-form-dialog',
   templateUrl: './live-form-dialog.component.html',
-  styleUrls: ['./live-form-dialog.component.css']
+  styleUrls: ['./live-form-dialog.component.css'],
 })
 export class LiveFormDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<LiveFormDialogComponent>) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  cancelar(): void {
+    this.dialogRef.close();
   }
-
 }
